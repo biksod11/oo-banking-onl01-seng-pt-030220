d70edd66db5d8e9580b94b71996281d6f28cfce2
Bikram Sodhi <biksodhi@gmail.com>
2020-03-31 01:04:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
-  # your code here
+
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender, @receiver, @amount = sender, reciever, amount
+    @status = pending
+  end
+
+  def valid?
+    sender
 end
